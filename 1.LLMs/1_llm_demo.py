060f2667118146fb94f311dcf4cb27f115cbfc59
sudogetshivam <sorealgalaxy@gmail.com>
2026-04-06 00:52:50 +0530

    langchain-exxploration

---
+++ 1.LLMs/1_llm_demo.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+
+
+load_dotenv()
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0  # Optional: keeps responses consistent
+)
+
+# 3. Invoke the model
+try:
+    response = llm.invoke("Explain blockchain in 30 words")
+    print(response.content)
+except Exception as e:
+    print(f"Error: {e}")
