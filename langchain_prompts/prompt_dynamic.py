61a5b432f3aa54e1a41bd7746bf0935de18c2fb8
sudogetshivam <sorealgalaxy@gmail.com>
2026-04-09 16:11:25 +0530

    make prompt more reusable for langchain echosystems

--- langchain_prompts/prompt_dynamic.py
+++ langchain_prompts/prompt_dynamic.py
@@ -2,11 +2,12 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 import os
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 import streamlit as st
 
 load_dotenv()
+
 
 st.header('Research Tool')
 
@@ -17,24 +18,7 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template = """
-Please summarize the research paper titled "{paper_input}" with the following specifications
-Explanation Style: {style_input}
-Explanation Length: {length_input}
-1. Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain the mathematical concepts using simple, intuitive code snippets where
-applicable.
-21 Analogies:
-- Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient
-information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.|
-""",
-input_variables = ['paper_input','style_input','length_input'],
-validate_template=True #jitne input_variables mentioned hain ya nahi usse validate karata hain, agar nahi hain toh error trigger hojayega
-)
+template = load_prompt('template.json')
 
 #fill the placeholders
 prompt = template.invoke({
