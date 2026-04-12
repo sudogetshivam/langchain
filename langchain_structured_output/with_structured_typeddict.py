2c6de142adc4a5d6b3fcb1a7e9253524f20416e6
sudogetshivam <sorealgalaxy@gmail.com>
2026-04-12 12:13:15 +0530

    generating structured_Schema output but in this the problem is there is no gaurantee the type you have defined in class, will return the same type of data by LLM

--- langchain_structured_output/with_structured_typeddict.py
+++ langchain_structured_output/with_structured_typeddict.py
@@ -1,7 +1,7 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os
-from typing import TypedDict,Annotated
+from typing import TypedDict,Annotated,Optional
 
 load_dotenv()
 
@@ -13,11 +13,30 @@ model = ChatGroq(
 
 #schema
 class Review(TypedDict):
+    key_themes: Annotated[list[str],"Write down all the key_themes disccused in the review in a list"]
     summary:Annotated[str,"A breif summary of review"]
     sentiment:Annotated[str,"return sentiment of the review either positive,negative or neutral"]
+    pros:Annotated[Optional[list[str]],"write down all the pros inside a list"]
+    cons:Annotated[Optional[list[str]],"write down all the cons inside a list"]
+    name:Annotated[Optional[str],'give me name of the reviewer']
 
 structured_model = model.with_structured_output(Review)
+response = structured_model.invoke("""I recently upgraded to the Samsung Galaxy S24 Ultra, and I must say, it's an absolute powerhouse! The Snapdragon 8 Gen 3processor makes everything lightning fast-whether I'm gaming, multitasking, or editing photos. The 5000mAh battery easily lasts a full day even with heavy use, and the 45W fast charging is a lifesaver. The S-Pen integration is a great touch for note-taking and quick sketches, though I don't use it often. What really blew me away is the 200MP camera-the night mode is stunning, capturing crisp, vibrant images even in low light. Zooming up to 100x
+actually works well for distant objects, but anything beyond 30x loses quality.
 
-response = structured_model.invoke("The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks qutdated compared to other brands. Hoping for a software update to fix this.")
+However, the weight and size make it a bit uncomfortable for one-handed use. Also, Samsung's One UI still comes with
+bloatware-why do I need five different Samsung apps for things Google already provides? The $1,300 price tag is also a hard
+pill to swallow.
+
+Pros:
+Insanely powerful processor (great for gaming and productivity)
+Stunning 200MP camera with incredible zoom capabilities
+Long battery life with fast charging
+S-Pen support is unique and useful
+
+Cons :
+Bulky and heavy-not great for one-handed use
+Bloatware still exists in One UI
+Expensive compared to competitor""")
 
 print(response)
